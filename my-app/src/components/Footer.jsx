dd45eef99767f81323c2d0c27061e7efcf2fc1e4
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-10 17:09:48 +0200

    Update Footer.jsx
    date function

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+  const date = new Date();
+  const fullYear = date.getFullYear();
+  
   return (
     <div className='footer'>
-      <p>&copy; 2025 Next Code. All rights reserved</p>
+      <p>&copy; {fullYear} Next Code. All rights reserved</p>
       <ul>
         <li>Terms of Services</li>
         <li>Privacy Policy</li>
